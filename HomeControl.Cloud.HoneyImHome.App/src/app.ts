f182b34c016c7e5ad57f396be9bd5239f44e9f8e
Oron Morad <oron.morad@gmail.com>
2016-02-20 03:11:19 +0200

    removing history navigation

--- HomeControl.Cloud.HoneyImHome.App/src/app.ts
+++ HomeControl.Cloud.HoneyImHome.App/src/app.ts
@@ -7,8 +7,7 @@ export class App {
         config.title = 'HIHome';
         config.map([
             { route: ['', 'login'], name: 'login', moduleId: './login', nav: true, title: 'Login' },
-            { route: 'whosehome', name: 'whosehome', moduleId: './whosehome', nav: true, title: 'Whose Home' },
-            { route: 'residentHistory', name: 'residentHistory', moduleId: './residentHistory', nav: true, title: 'History' }
+            { route: 'whosehome', name: 'whosehome', moduleId: './whosehome', nav: true, title: 'Whose Home' }
         ]);
 
         this.router = router;
